85b71dfe5b54037c66b5f2ac88a269f3053bfc29
Mingwei Tian <mingwei.tian@microsoft.com>
2014-05-08 14:42:41 -0700

    Copied from AzureSiteReplicator

---
+++ AzureSiteReplicator/Environment.cs
@@ -0,0 +1,81 @@
+﻿using AzureSiteReplicator.Contracts;
+using AzureSiteReplicator.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace AzureSiteReplicator
+{
+    public class Environment : IEnvironment
+    {
+        private static IEnvironment s_instance = null;
+        private static Object lockObj = new Object();
+
+        public static IEnvironment Instance
+        {
+            get
+            {
+                if (s_instance == null)
+                {
+                    lock (lockObj)
+                    {
+                        if (s_instance == null)
+                        {
+                            s_instance = new Environment();
+                        }
+                    }
+                }
+
+                return s_instance;
+            }
+
+            set
+            {
+                lock (lockObj)
+                {
+                    s_instance = value;
+                }
+            }
+        }
+
+        public Environment()
+        {
+            string homePath = System.Environment.ExpandEnvironmentVariables(@"%SystemDrive%\home");
+
+            if (Directory.Exists(homePath))
+            {
+                // Running on Azure
+
+                // Publish the wwwroot folder
+                ContentPath = Path.Combine(homePath, "site", "wwwroot");
+
+                SiteReplicatorPath = Path.Combine(homePath, "data", "SiteReplicator");
+            }
+            else
+            {
+                // Local case: run from App_Data for testing purpose
+
+                string appData = HostingEnvironment.MapPath("~/App_Data");
+
+                ContentPath = Path.Combine(appData, "source");
+
+                SiteReplicatorPath = Path.Combine(appData, "SiteReplicator");
+            }
+
+            Trace.TraceInformation("ContentPath={0}", ContentPath);
+            Directory.CreateDirectory(ContentPath);
+            Trace.TraceInformation("SiteReplicator={0}", SiteReplicatorPath);
+            Directory.CreateDirectory(SiteReplicatorPath);
+        }
+
+        // Path to the web content we want to replicate
+        public string ContentPath { get; set; }
+
+        // Path to the publish settings files that drive where we publish to
+        public string SiteReplicatorPath { get; set; }
+    }
+}
---
+++ AzureSiteReplicator/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using AzureSiteReplicator.Controllers;
+using AzureSiteReplicator.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace AzureSiteReplicator
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            // Trigger an initial deployment when we start
+            Replicator.Instance.TriggerDeployment();
+        }
+    }
+}
